5aa390587e737b4a75569f8847ef5a3558a84be2
IvanYou98 <yuanfanyou@gmail.com>
2022-03-23 22:22:43 -0400

    Finish routes

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const PORT = process.env.PORT || 8720;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-// set up the router
 app.get('/', ((req, res) => {
     res.send('API running');
 }))
+
+
+// define route
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/profile', require('./routes/api/profile'))
