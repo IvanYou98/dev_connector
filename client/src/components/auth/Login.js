023df81c43c141cbc2b31f6296471421b8a5c5c4
IvanYou98 <yuanfanyou@gmail.com>
2022-03-25 20:21:16 -0400

    finish log in and register in react

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
 
-function Login(props) {
+const Login = (props) => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const {email, password} = formData;
+
+    const onChange = e => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log(formData)
+    }
+
     return (
         <div>
             <section className="container">
-                <div className="alert alert-danger">
-                    Invalid credentials
-                </div>
+                {/*<div className="alert alert-danger">*/}
+                {/*    Invalid credentials*/}
+                {/*</div>*/}
                 <h1 className="large text-primary">Sign In</h1>
-                <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
-                <form className="form" action="dashboard.html">
+                <p className="lead"><i className="fas fa-user"/> Sign into Your Account</p>
+                <form className="form" action="dashboard.html" onSubmit={e => onSubmit(e)}>
                     <div className="form-group">
                         <input
                             type="email"
                             placeholder="Email Address"
                             name="email"
+                            value={email}
                             required
+                            onChange={e => onChange(e)}
                         />
                     </div>
                     <div className="form-group">
@@ -23,12 +45,14 @@ function Login(props) {
                             type="password"
                             placeholder="Password"
                             name="password"
+                            value={password}
+                            onChange={e => onChange(e)}
                         />
                     </div>
                     <input type="submit" className="btn btn-primary" value="Login"/>
                 </form>
                 <p className="my-1">
-                    Don't have an account? <a href="register.html">Sign Up</a>
+                    Don't have an account? <Link to='/register'>Sign Up</Link>
                 </p>
             </section>
         </div>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
+import {Link} from "react-router-dom";
 
-function Register(props) {
+
+const Register = (props) => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
+
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        if (password !== password2) {
+            console.log('Passwords do not match');
+        } else {
+            console.log(formData);
+            console.log("SUCCESS");
+        }
+    }
+
+    const {name, email, password, password2} = formData;
     return (
         <section className="container">
             <h1 className="large text-primary">Sign Up</h1>
             <p className="lead"><i className="fas fa-user"/> Create Your Account</p>
-            <form className="form" action="create-profile.html">
+            <form className="form" action="create-profile.html" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
-                    <input type="text" placeholder="Name" name="name" required/>
+                    <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)}
+                           required/>
                 </div>
                 <div className="form-group">
-                    <input type="email" placeholder="Email Address" name="email"/>
+                    <input type="email" placeholder="Email Address" name="email" value={email}
+                           onChange={e => onChange(e)}/>
                     <small className="form-text"
                     >This site uses Gravatar so if you want a profile image, use a
                         Gravatar email</small
@@ -21,6 +46,8 @@ function Register(props) {
                         type="password"
                         placeholder="Password"
                         name="password"
+                        value={password}
+                        onChange={e => onChange(e)}
                         minLength="6"
                     />
                 </div>
@@ -29,13 +56,16 @@ function Register(props) {
                         type="password"
                         placeholder="Confirm Password"
                         name="password2"
+                        value={password2}
+                        onChange={e => onChange(e)}
                         minLength="6"
                     />
                 </div>
                 <input type="submit" className="btn btn-primary" value="Register"/>
             </form>
             <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
+                Already have an account?
+                <Link to="/login">Sign In</Link>
             </p>
         </section>
     );
