7af594b99459befe73dcede1554d68aa543436ca
IvanYou98 <yuanfanyou@gmail.com>
2022-04-03 14:16:11 -0400

    config for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors')
+const path = require('path')
 
 const app = express();
 app.use(cors())
@@ -9,13 +10,9 @@ app.use(express.json())
 connectDB();
 
 
-const PORT = process.env.PORT || 8720;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-app.get('/', ((req, res) => {
-    res.send('API running');
-}))
+// app.get('/', ((req, res) => {
+//     res.send('API running');
+// }))
 
 
 // define route
@@ -23,3 +20,14 @@ app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/profile', require('./routes/api/profile'))
+
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+const PORT = process.env.PORT || 8720;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
